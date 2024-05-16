b3d0d53a9e2cc1a7d18c1c29e5add417e2f6d115
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2024-05-16 09:00:19 -0300

    Atualizando desafio_3

--- Desafio 3/js-curso-2-aula_3/app.js
+++ Desafio 3/js-curso-2-aula_3/app.js
@@ -1,71 +1,90 @@
-let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
+// let numeroSecreto = gerarNumeroAleatorio();
+// let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-}
+// function exibirTextoNaTela(tag, texto) {
+//     let campo = document.querySelector(tag);
+//     campo.innerHTML = texto;
+// }
 
-function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-}
+// function exibirMensagemInicial() {
+//     exibirTextoNaTela('h1', 'Jogo do número secreto');
+//     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+// }
 
-exibirMensagemInicial();
+// exibirMensagemInicial();
 
-function verificarChute() {
-    let chute = document.querySelector('input').value;
+// function verificarChute() {
+//     let chute = document.querySelector('input').value;
     
-    if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou! ;)');
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-        exibirTextoNaTela('p', mensagemTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled')
-    } else {
-        if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secreto é menor');
-        } else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
-        }
-        tentativas++; 
-        // Mesmo que: tentativas = tentativas + 1;
-        limparCampo();
-    }
+//     if (chute == numeroSecreto) {
+//         exibirTextoNaTela('h1', 'Acertou! ;)');
+//         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativas';
+//         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+//         exibirTextoNaTela('p', mensagemTentativas);
+//         document.getElementById('reiniciar').removeAttribute('disabled')
+//     } else {
+//         if (chute > numeroSecreto) {
+//             exibirTextoNaTela('p', 'O número secreto é menor');
+//         } else {
+//             exibirTextoNaTela('p', 'O número secreto é maior');
+//         }
+//         tentativas++; 
+//         // Mesmo que: tentativas = tentativas + 1;
+//         limparCampo();
+//     }
 
-}
+// }
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
-}
+// function gerarNumeroAleatorio() {
+//     return parseInt(Math.random() * 10 + 1);
+// }
 
-function limparCampo() {
-    chute = document.querySelector('input');
-    chute.value = '';
-}
+// function limparCampo() {
+//     chute = document.querySelector('input');
+//     chute.value = '';
+// }
 
-function reiniciarJogo() {
-    numeroSecreto = gerarNumeroAleatorio();
-    limparCampo();
-    tentativas = 1;
-    exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true);
-}
+// function reiniciarJogo() {
+//     numeroSecreto = gerarNumeroAleatorio();
+//     limparCampo();
+//     tentativas = 1;
+//     exibirMensagemInicial();
+//     document.getElementById('reiniciar').setAttribute('disabled', true);
+// }
 
-// 1) Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura,
-// em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+// // 1) Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura,
+// // em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
 
-function calculoImc(peso, altura) {
-    return resultadoImc = peso / (altura * altura);
-}
+// function calculoImc(peso, altura) {
+//     return resultadoImc = peso / (altura * altura);
+// }
 
-calculoImc(96, 1.77);
-console.log(resultadoImc);
+// calculoImc(96, 1.77);
+// console.log(resultadoImc);
 
-// 2) Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+// // 2) Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
+// let resultadoFatorial;
+
+// function calcularFatorial(numero) {
+//    return numero <= 1 ? 1 : numero * calcularFatorial(numero - 1);
+// }
+
+// resultadoFatorial = calcularFatorial(5);
+// console.log(resultadoFatorial);
 
 // 3) Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente
 // em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+
+let reais;
+
+function conversor(dolar) {
+    return dolar * 4.80;
+}
+
+conversor(100);
+reais = conversor(100);
+console.log(reais);
 
 // 4) Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e 
 // largura que serão dadas como parâmetro.
