1bda90b4b84e4876ac0f92375487f86cd0f9f4fd
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2024-05-21 12:40:54 -0300

    correções

--- Desafio 3/js-curso-2-aula_3/app.js
+++ Desafio 3/js-curso-2-aula_3/app.js
@@ -73,23 +73,44 @@
 // resultadoFatorial = calcularFatorial(5);
 // console.log(resultadoFatorial);
 
-// 3) Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente
-// em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+// // 3) Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente
+// // em reais. Para isso, considere a cotação do dólar igual a R$4,80.
 
-let reais;
+// let reais;
 
-function conversor(dolar) {
-    return dolar * 4.80;
-}
+// function conversor(dolar) {
+//     return dolar * 4.80;
+// }
 
-conversor(100);
-reais = conversor(100);
-console.log(reais);
+// conversor(100);
+// reais = conversor(100);
+// console.log(`O valor convertido de $ 100,00 dolares para reais fica então R$${reais},00`);
 
-// 4) Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e 
-// largura que serão dadas como parâmetro.
+// // 4) Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e 
+// // largura que serão dadas como parâmetro.
+
+
+// function calcularLados(B, b) {
+//    let area = B * b;
+//    let perimetro = 2*B + 2*b;
+
+//     alert(`A área do retangulo é ${area} e o perimetro do retangulo é ${perimetro}`);
+// }
+
+// calcularLados(5, 3);
 
 // 5) Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que
 // será fornecido como parâmetro. Considere Pi = 3,14.
+
+let pi = 3.14;
+
+function calcularRaio(raio) {
+    let areaCirculo = pi * (raio * raio);
+    let perimetroCirculo = 2 * pi * raio;
+   
+   console.log(`A área do círculo é ${areaCirculo}m², e o perímetro é ${perimetroCirculo} `);
+}
+
+calcularRaio(2);
 
 // 6) Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
