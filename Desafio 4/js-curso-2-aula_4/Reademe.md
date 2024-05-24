8026b30b5bcc787c8df615dde797dfd2f1a5e883
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2024-05-24 11:15:04 -0300

    Aula_4 e desafio_4 finalizado

---
+++ Desafio 4/js-curso-2-aula_4/Reademe.md
@@ -0,0 +1,48 @@
+# Resolução dos desafios 2
+
+Entender listas em programação é fundamental, pois elas representam uma maneira eficaz de organizar e manipular conjuntos de dados relacionados. Uma lista, também conhecida como array em muitas linguagens de programação, permite armazenar múltiplos valores em uma única estrutura, o que simplifica o acesso e a gestão dos dados. A importância de compreender listas reside na capacidade de lidar com coleções de informações de maneira sistemática, permitindo que programas realizem tarefas como armazenamento, busca, ordenação e processamento de dados de forma eficiente e estruturada.
+
+Pensando nisso, criamos uma lista de atividades (não obrigatórias) focada em prática para melhorar ainda mais sua experiência de aprendizagem.
+[]()
+
+#### Sugestões de respostas
+
+1) Crie uma lista vazia, com o nome `listaGenerica`.
+```js
+let listaGenerica = [];
+```
+
+2) Crie uma lista de linguagens de programação chamada `linguagensDeProgramacao`.
+
+```js
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+```
+
+3) Adicione à lista `linguagensDeProgramacao` os seguintes elementos: Java, Ruby e GoLang.
+
+```js
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+// Lista ao final: ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+```
+
+4) Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+```js
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+```
+
+5) Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+```js
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+```
+
+6) Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+```js
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
+```
--- Desafio 4/js-curso-2-aula_4/app.js
+++ Desafio 4/js-curso-2-aula_4/app.js
@@ -1,8 +1,10 @@
 // Nessa aula tivemos o primeiro contato com a tag "Array".
 let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -18,7 +20,7 @@ exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    
+
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
@@ -37,9 +39,14 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * 3 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let = quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
 
-    if (listaDeNumerosSorteados.includes(numeroEscolhido)) { // o atributo "include" verifica se o elementos já foi incluido na listaDeNumerosSoteados e retorna como true ou false.
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) { // o atributo "includes" verifica se o elementos já foi incluido na listaDeNumerosSoteados e retorna como true ou false.
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido); // o atibuto "push" adiciona um item ao final da lista.
@@ -61,9 +68,41 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
+// 1) Crie uma lista vazia, com o nome listaGenerica.
 
+let listaGenerica = [];
 
+// 2) Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
 
+let linguagemDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3) Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+linguagemDeProgramacao.push('Java');
+linguagemDeProgramacao.push('Ruby');
+linguagemDeProgramacao.push('GoLang');
+// Nesse caso específico eu poderia fazer em uma única linha de comando como no exemplo abaixo:
+// linguagemDeProgramacao.push('Java', 'Ruby', 'GoLand');
+
+console.log(linguagemDeProgramacao);
+
+// 4) Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+let nomesFamilia = ['Marcelo', 'Elaine', 'Braz'];
+console.log(nomesFamilia [0]);
+console.log(nomesFamilia.length);
+
+// 5) Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+let nomesFamiliaEla = ['João Paulo', 'Aline', 'Amanda'];
+console.log(nomesFamiliaEla [1]);
+console.log(nomesFamiliaEla.length);
+
+// 6) Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+let nomesAmigos = ['Felipe', 'Tavinho', 'Júlio'];
+console.log(nomesAmigos[2]);
+console.log(nomesAmigos.length);
 
 
 
